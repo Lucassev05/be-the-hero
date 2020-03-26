9fce552adcd5b59b642c1c6955f7133ab5dca2e1
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-03-26 17:04:13 -0300

    Updates

---
+++ precache-manifest.3cf029eff08199ffeab03696fbe32e5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86169077638f3dc213768cd8d01e5dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9757d72d4a8f184fec4",
+    "url": "/static/css/main.f1e13be0.chunk.css"
+  },
+  {
+    "revision": "021baf7baea1f456c9c0",
+    "url": "/static/js/2.c4c075dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4c075dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9757d72d4a8f184fec4",
+    "url": "/static/js/main.9773ac96.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
